da9e79f945eaa89d0c59f5978db182e8e8b0aba8
202-11 <202-11@202-11-PC>
2016-07-15 16:12:47 +0900

    수정2

--- r1/src/Ryu.java
+++ r1/src/Ryu.java
@@ -1,11 +1,7 @@
 public class Ryu {
 	public static void main(String[] args) {
-		System.out.println("ÇĎŔĚżë");
-		System.out.println("¤ž¤ž");
-		System.out.println("kk");
-		System.out.println("kadssadads");
-		System.out.println("kadssadads");
-		System.out.println("hello");
+		System.out.println("����1");
+		
 	}
 
 }
